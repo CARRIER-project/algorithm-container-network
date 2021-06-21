f19aa9b8937ba87e2c888355c8920ac13df6b854
Djura Smits <djura.smits@gmail.com>
2021-06-21 10:28:34 +0200

    Created basic docker compose setup. Algorithm containers not able to use network yet though

---
+++ secondary-algorithm/secondary.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env/ python3
+
+import asyncio
+
+DEFAULT_PORT = 8888
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close()
+
+
+async def main(port=DEFAULT_PORT):
+    server = await asyncio.start_server(
+        handle_echo, '127.0.0.1', port)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main)
