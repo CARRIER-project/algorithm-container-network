7e6b6101575ad08a6a116ab7a7c0ab0346c55cb2
Djura Smits <djura.smits@gmail.com>
2021-07-05 12:39:41 +0200

    Custom vpn client image

---
+++ vpn-client/entry.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Iptables
+
+# Routing
+
+
+# Nat
+
+# Run vpn
+
+echo "Starting vpn client..."
+
+openvpn --config "$VPN_CONFIG" \
+    --connect-retry-max 10 \
+    --pull-filter ignore "route-ipv6" \
+    --pull-filter ignore "ifconfig-ipv6" \
+    --script-security 2 \
+    --up-restart \
+    --cd /data &
+openvpn_child=$!
+
+wait $openvpn_child
