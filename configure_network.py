26685167cce545837c15d0ec128004e43eb6f261
Djura Smits <djura.smits@gmail.com>
2021-08-11 14:02:26 +0200

    Documentation

--- configure_network.py
+++ configure_network.py
@@ -1,6 +1,5 @@
 from typing import List, Union, Dict
 
-import clize
 import docker
 from docker.errors import NotFound
 from docker.models.networks import Network
@@ -8,11 +7,13 @@ import json
 
 _client = None
 
-_DEFAULT_BASE_IMAGE = 'algorithm-base'
+# FIXME: This image can only be found if the dockerfile in the folder `network-config/` has been
+#  built with the  appropriate tag.
+_NETWORK_CONFIG_IMAGE = 'network-config'
 _HOST = 'host'
 
 
-def get_client() -> docker.client.DockerClient:
+def _get_client() -> docker.client.DockerClient:
     global _client
 
     if not _client:
@@ -21,7 +22,7 @@ def get_client() -> docker.client.DockerClient:
 
 
 def create_network(name):
-    client = get_client()
+    client = _get_client()
 
     if network_exists(name):
         raise Exception('Network already exists!')
@@ -33,19 +34,25 @@ def create_network(name):
 
 
 def find_isolated_bridge(container_id: str):
+    """
+    Retrieve the linked network interface in the host namespace for network interface eth0 in the
+    container namespace.
+
+    :param container_id: container id of the docker container connected to an isolated network.
+    :return: the name of the network interface in the host namespace
+    """
     # Get network config from isolated container namespace
-    container = get_client().containers.get(container_id=container_id)
+    container = _get_client().containers.get(container_id=container_id)
     _, isolated_interface = container.exec_run(['ip', '--json', 'addr', 'show', 'dev', 'eth0'])
 
     isolated_interface = json.loads(isolated_interface)
 
-    if_config = _flatten_if_config(isolated_interface)
-    link_index = _get_link_index(if_config)
+    link_index = _get_link_index(isolated_interface)
 
     # Get network config from host namespace
-    host_interfaces = get_client().containers.run(_DEFAULT_BASE_IMAGE,
-                                                  network=_HOST,
-                                                  command=['ip', '--json', 'addr'])
+    host_interfaces = _get_client().containers.run(_NETWORK_CONFIG_IMAGE,
+                                                   network=_HOST,
+                                                   command=['ip', '--json', 'addr'])
 
     host_interfaces = json.loads(host_interfaces)
 
@@ -72,17 +79,28 @@ def _get_if(interfaces, index) -> Union[Dict, None]:
 
 
 def _get_link_index(if_json: Union[Dict, List]) -> int:
+    if isinstance(if_json, list):
+        if_json = if_json[-1]
     return int(if_json['link_index'])
 
 
 def configure_host_namespace(vpn_subnet: str, isolated_bridge: str):
-    command = f'iptables -I DOCKER-USER 1 -d {vpn_subnet} -i {isolated_bridge} -j ACCEPT &&'
-    f'iptables -I DOCKER-USER 1 -s $vpn_subnet -o $isolated_bridge -j ACCEPT'
+    """
+    By default the internal bridge networks are configured to prohibit packet forwarding between
+    networks. Create an exception to this rule for forwarding traffic between the bridge and vpn
+    network.
 
-    get_client().containers.run(_DEFAULT_BASE_IMAGE,
-                                network='host',
-                                cap_add='NET_ADMIN',
-                                command=command)
+    :param vpn_subnet:
+    :param isolated_bridge:
+    :return:
+    """
+    command = f'sh -c "iptables -I DOCKER-USER 1 -d {vpn_subnet} -i {isolated_bridge} -j ACCEPT ' \
+              f'; iptables -I DOCKER-USER 1 -s {vpn_subnet} -o {isolated_bridge} -j ACCEPT "'
+
+    _get_client().containers.run(_NETWORK_CONFIG_IMAGE,
+                                 network='host',
+                                 cap_add='NET_ADMIN',
+                                 command=command)
 
 
 def get_subnet(network: Network):
@@ -91,11 +109,7 @@ def get_subnet(network: Network):
 
 def network_exists(name):
     try:
-        get_client().networks.get(name)
+        _get_client().networks.get(name)
     except NotFound:
         return False
     return True
-
-
-if __name__ == '__main__':
-    clize.run(create_network)
